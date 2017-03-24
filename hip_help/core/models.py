9d3ec80f4cec374bf7198467832de8b5148735f2
Bartłomiej Bęczkowski <beczkowb@gmail.com>
2017-03-24 20:32:52 +0100

    adding core app and basic model for handling communication between hipchat and bot

---
+++ hip_help/core/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+
+class Installation(models.Model):
+    oauth_id = models.CharField(max_length=100)
+    capabilities_url = models.CharField(max_length=100)
+    room_id = models.IntegerField()
+    group_id = models.IntegerField()
+    oauth_secret = models.CharField(max_length=100)
+
+    authorization_url = models.URLField(blank=True)
+    token_url = models.URLField(blank=True)
+    api_url = models.URLField(blank=True)
+
+
+class AccessToken(models.Model):
+    installation = models.OneToOneField('Installation')
+
+    expiration_timestamp = models.DateTimeField()
+    token = models.CharField(max_length=100)
