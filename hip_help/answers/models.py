e41e39b3d4c16608ae6747326f52cd4496b6ffdd
daniel.faderski <daniel.faderski@jmr.pl>
2017-03-25 00:40:37 +0100

    save_records function for creating answers objects

--- hip_help/answers/models.py
+++ hip_help/answers/models.py
@@ -13,16 +13,34 @@ class Answer(models.Model):
 
     class Meta:
         unique_together = ('installation', 'keyword')
+    #
+    # @classmethod
+    # def save_records(cls, records_dict):
+    #     cls.objects.all().delete()
+    #     room_name_set = set()
+    #     for room_name, _ in records_dict:
+    #         room_name_set.add(room_name)
+    #     room_name_list = list(room_name_set)
+    #     room_objects = Installation.objects.filter(group_id__in=int(room_name_list))
+    #     room_dict = {room.room_name: room.room_id for room_objects}
 
+    # TODO optimize and change for real room names (ids currently)
     @classmethod
     def save_records(cls, records_dict):
-        cls.objects.all().delete()
-        room_name_set = set()
-        for room_name, _ in records_dict:
-            room_name_set.add(room_name)
-        room_name_list = list(room_name_set)
-        room_objects = Installation.objects.filter(group_id__in=int())
+        for room_id, answers_list in records_dict.items():
+            try:
+                room = Installation.objects.get(id=int(room_id))
+            except models.ObjectDoesNotExist:
+                continue
 
+            answer_object_list = []
+            for answer in answers_list:
+                answer_object_list.append(Answer(
+                    installation=room,
+                    keyword=answer['keyword'],
+                    text=answer['text']
+                ))
+            cls.objects.bulk_create(answer_object_list)
 
     def __str__(self):
         return self.keyword
