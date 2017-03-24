77a2a8d36e8210616e6d84fe8ef826ed5e34a3d5
daniel.faderski <daniel.faderski@jmr.pl>
2017-03-24 22:06:35 +0100

    Altering keyword field as unique.

--- hip_help/answers/models.py
+++ hip_help/answers/models.py
@@ -3,7 +3,7 @@ from django.db.models import Q
 
 
 class Answer(models.Model):
-    keyword = models.CharField(max_length=30, db_index=True)
+    keyword = models.CharField(max_length=30, unique=True)
     text = models.TextField()
     installation = models.ForeignKey('core.Installation')
 
