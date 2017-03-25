78eb4b7f0041915c336d7cd7621c8455b1db85d3
Daniel Malik <daniel.malik.malik@gmail.com>
2017-03-25 03:32:43 +0100

    get data from git

--- hip_help/answers/utils.py
+++ hip_help/answers/utils.py
@@ -1,7 +1,25 @@
 import json
+import os
+from git import Repo
 
 
 def open_answers(filename):
     with open(filename) as data_file:
         data = json.load(data_file)
     return data
+
+def get_repo_from_git(url, path, filename):
+
+    if os.path.exists(path):
+        repo = Repo(path).remotes.origin.pull()
+    else:
+        Repo.clone_from(url, path)
+    for root, dirs, files in os.walk(path):
+        print(files)
+        if filename in files:
+            result = os.path.join(root, filename)
+
+            with open(result) as data_file:
+                data = json.load(data_file)
+            return data
+
